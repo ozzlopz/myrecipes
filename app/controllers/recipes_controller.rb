f006db6dada7399ac5489f2f116b147015f62a50
ozzlopz <ozzlopz@gmail.com>
2015-05-03 04:54:58 +0000

    Add admin functionality

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,8 +2,9 @@ class RecipesController < ApplicationController
   
   before_action :set_recipe, only: [:edit,:update,:show,:like] 
   before_action :require_user, except: [:show, :index, :like]
-   before_action :require_user_like, only: [:like]
+  before_action :require_user_like, only: [:like]
   before_action :require_same_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
   
   
   def index
@@ -58,6 +59,12 @@ class RecipesController < ApplicationController
     end
   end
   
+  def destroy
+    Recipe.find(params[:id]).destroy
+    flash[:success] = "Recipe deleted"
+    redirect_to recipes_path
+  end
+  
   private
   
     def recipe_params
@@ -69,7 +76,7 @@ class RecipesController < ApplicationController
     end
     
     def require_same_user
-      if current_user != @recipe.chef
+      if current_user != @recipe.chef and !current_user.admin?
         flash[:danger] = "You can only edit your own recipes"
         redirect_to root_path
       end
@@ -80,5 +87,9 @@ class RecipesController < ApplicationController
         flash[:danger] = "You must be logged in to perform that action"
         redirect_to :back
       end
+    end
+    
+    def admin_user
+      redirect_to recipes_path unless current_user.admin?
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150502185720) do
+ActiveRecord::Schema.define(version: 20150503042507) do
 
   create_table "chefs", force: true do |t|
     t.string   "chefname"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150502185720) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
   create_table "ingredients", force: true do |t|
